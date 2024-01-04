ced28615f3a9575a0e64326370da420092951654
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-04 02:44:30 +0100

    Added country cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import logo from '/images/techover-logo.png';
 function Navbar() {
 	return (
 		<nav className="navbar">
+			<h3>The Flag App</h3>
 			<img className="logo" src={logo} alt="Techover" />
 			<div className="nav-links">
 				<NavLink className="nav-link" to="/">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import data from '../data.json';
+
 export default function HomePage() {
 	return (
 		<div className="home-page">
-			<h1>Home Page</h1>
+			{data.map((item) => (
+				<div className="card">
+					{/* <img src={item.flag} alt="flag" /> */}
+					<div className="card-body">
+						<h2>{item.name}</h2>
+						<p>
+							<strong>Population:</strong> {item.population}
+						</p>
+						<p>
+							<strong>Region:</strong> {item.region}
+						</p>
+						<p>
+							<strong>Capital:</strong> {item.capital}
+						</p>
+					</div>
+				</div>
+			))}
 		</div>
 	);
 }
