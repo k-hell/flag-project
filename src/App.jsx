b76d38e2767b1337a11cd61974220abdabe679fe
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-06 11:01:17 +0100

    Minor styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CountryPage from './pages/CountryPage';
