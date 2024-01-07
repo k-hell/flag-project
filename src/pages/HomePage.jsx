8ee288570baa628a8d0900b6f5a4b52ff4655853
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-07 08:35:03 +0100

    Changed classname for filter dropdown

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
 	return (
 		<div className="home-page">
 			<div className="searchbar">Search for a country...</div>
-			<div className="filterbyregion">Filter by Region</div>
+			<div className="filterdropdown">Filter by Region</div>
 			<CountryCard />
 		</div>
 	);
