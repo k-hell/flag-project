f5565e045bc22b3b1943bab41830e7a16c9331df
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-14 19:22:58 +0100

    Added my media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	darkMode: 'class',
 	theme: {
+		screens: {
+			'3card': { max: '1259px' },
+			'2card': { max: '939px' },
+			'1card': { max: '659px' }
+		},
 		extend: {
 			colors: {
 				'dark-blue': '#2b3844',		// darkblue
