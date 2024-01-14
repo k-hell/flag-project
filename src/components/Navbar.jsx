6d9551862f81d8c9e3e5a28eebc3600637cbc695
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-15 00:07:35 +0100

    Added moon theme changer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import lightLogo from '/images/techover-logo.png';
-import darkLogo from '/images/techover-logo-dark.png';
-import ThemeBtn from './ThemeBtn';
 import useTheme from '../contexts/theme';
 
 function Navbar() {
-	const { themeMode } = useTheme();
+	const { themeMode, lightTheme, darkTheme } = useTheme();
+
+	const onClickBtn = () => {
+		if (themeMode === 'light') {
+			darkTheme();
+		} else {
+			lightTheme();
+		}
+	};
 
 	return (
 		<nav className="navbar flex justify-between items-center bg-white h-[100px] fixed top-0 w-full dark:bg-dark-blue">
@@ -18,13 +23,25 @@ function Navbar() {
 			</NavLink>
 			<div>
 				{themeMode === 'light' ? (
-					<img className="darkLogo 1card:hidden" src={darkLogo} alt="Techover" />
+					<img className="darkLogo 1card:hidden" src="./images/techover-logo-dark.png" alt="Techover" />
 				) : (
-					<img className="lightLogo 1card:hidden" src={lightLogo} alt="Techover" />
+					<img className="lightLogo 1card:hidden" src="./images/techover-logo.png" alt="Techover" />
 				)}
 			</div>
-			<div className="nav-links flex gap-4 justify-end items-center max-w-[1200px] h-[30px] mr-[25px]">
-				<ThemeBtn />
+			<div
+				className="nav-links flex gap-4 justify-end items-center max-w-[1200px] h-[30px] mr-[25px] cursor-pointer"
+				onClick={onClickBtn}
+			>
+				<div>
+					{themeMode === 'light' ? (
+						<img src="./images/moon-bordered.svg" alt="Theme Changer" />
+					) : (
+						<img src="./images/moon.svg" alt="Theme Changer" />
+					)}
+				</div>
+				<p className="text-darker-blue dark:text-to-white">
+					{themeMode === 'light' ? 'Dark Mode' : 'Light Mode'}
+				</p>
 			</div>
 		</nav>
 	);
