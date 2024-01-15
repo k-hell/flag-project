be94dc075d5a6db82764296618e696ed64dd2b29
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-15 16:55:27 +0100

    Fixed SearchBar text coloring and tried to prettier-plugin for tailwind

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,7 @@ const SearchBar = ({ value, onChange }) => {
 			placeholder="Search for a country..."
 			value={value}
 			onChange={onChange}
-			className="searchbar h-[50px] w-[470px] rounded-xl bg-white flex justify-start items-center p-[30px] border-none dark:bg-dark-blue 2card:w-[250px] 1card:h-[70px] 1card:w-[350px] 1card:m-0 1card:mx-auto"
+			className="searchbar h-[50px] w-[470px] rounded-xl bg-white flex justify-start items-center p-[30px] border-none text-darker-blue dark:text-to-white dark:bg-dark-blue 2card:w-[250px] 1card:h-[70px] 1card:w-[350px] 1card:m-0 1card:mx-auto"
 		/>
 	);
 };
