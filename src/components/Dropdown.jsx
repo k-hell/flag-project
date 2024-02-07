92acfff4eded500c3976d63cb2a58e28abbc3231
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-07 10:36:32 +0100

    Small change in dropdown component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -15,24 +15,24 @@ const Dropdown = ({ handleRegionChange }) => {
 				)}
 			</div>
 			<div className="dropdown-content">
-				<a className="dropdown-link rounded-t-xl" onClick={() => handleRegionChange(null)}>
+				<div className="dropdown-link rounded-t-xl" onClick={() => handleRegionChange(null)}>
 					All
-				</a>
-				<a className="dropdown-link" onClick={() => handleRegionChange('Americas')}>
+				</div>
+				<div className="dropdown-link" onClick={() => handleRegionChange('Americas')}>
 					Americas
-				</a>
-				<a className="dropdown-link" onClick={() => handleRegionChange('Asia')}>
+				</div>
+				<div className="dropdown-link" onClick={() => handleRegionChange('Asia')}>
 					Asia
-				</a>
-				<a className="dropdown-link" onClick={() => handleRegionChange('Europe')}>
+				</div>
+				<div className="dropdown-link" onClick={() => handleRegionChange('Europe')}>
 					Europe
-				</a>
-				<a className="dropdown-link" onClick={() => handleRegionChange('Africa')}>
+				</div>
+				<div className="dropdown-link" onClick={() => handleRegionChange('Africa')}>
 					Africa
-				</a>
-				<a className="dropdown-link rounded-b-xl" onClick={() => handleRegionChange('Oceania')}>
+				</div>
+				<div className="dropdown-link rounded-b-xl" onClick={() => handleRegionChange('Oceania')}>
 					Oceania
-				</a>
+				</div>
 			</div>
 		</div>
 	);
